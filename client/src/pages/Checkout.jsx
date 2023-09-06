fab5bc9fb64de890d08babb30c63e73dc4cfb439
Tulipreactjsmain <rdhnft@gmail.com>
2023-09-06 15:11:39 +0100

    Checkout UpdAte

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { PageLayout, Headings } from "../component";
+import { PageLayout, Headings } from "../components";
 import { Button, Row, Col, Spinner } from "react-bootstrap";
 import { toast } from "react-hot-toast";
 import { useForm } from "react-hook-form";
@@ -11,12 +11,13 @@ import { createOrder } from "../config/api";
 
 export default function Checkout() {
   const {
-    shippingDetails,
     setShippingDetails,
+    shippingDetails,
     paymentMethod,
     setPaymentMethod,
     currentUser,
     priceTotal,
+    cartItems,
   } = useStore();
   const {
     register,
@@ -33,9 +34,10 @@ export default function Checkout() {
 
   useEffect(() => {
     document.title = "Checkout";
-  });
+  }, []);
 
   const paymentOptions = [{ name: "Cash" }, { name: "Paypal" }];
+
   const tax = 0.05;
   const calcTax = (tax * priceTotal).toFixed(2);
   const calcShippingFee = (priceTotal / 2) * tax;
@@ -46,9 +48,104 @@ export default function Checkout() {
     Number(calcTax) +
     Number(shippingFee)
   ).toFixed(2);
+
   return (
     <PageLayout>
       <Headings title="Checkout" />
+      {cartItems?.length > 0 ? (
+        <Row className="justify-content-between">
+          <Col md={6} lg={5} className="mb-5">
+            <h1 className="fs-4">Order summary</h1>
+            {cartItems.map((item) => (
+              <Row key={item._id} className="py-2">
+                <Col md={6}>
+                  <p className="mb-0 fs-6">{item.title}</p>
+                </Col>
+                <Col md={2}>
+                  <p className="mb-0 fs-6">Qty: {item.quantity}</p>
+                </Col>
+                <Col md={4}>
+                  <p className="mb-0 fs-6">
+                    Price: {formatCurrency(item.price)}
+                  </p>
+                </Col>
+              </Row>
+            ))}
+            <hr />
+            <div className="mt-4">
+              <div className="d-flex justify-content-between mb-2 fs-6">
+                <p className="mb-0">Subtotal:</p>
+                <p>{formatCurrency(priceTotal)}</p>
+              </div>
+              <div className="d-flex justify-content-between mb-2 fs-6">
+                <p className="mb-0">Tax</p>
+                <p className="mb-0 text-success">Fixed 0.5</p>
+                <p>{formatCurrency(calcTax)}</p>
+              </div>
+              <div className="d-flex justify-content-between mb-2 fs-6">
+                <p className="mb-0"> Shipping Fee</p>
+
+                <p>{formatCurrency(shippingFee)}</p>
+              </div>
+              <hr />
+              <div className="d-flex justify-content-between mb-2 fs-6">
+                <p className="mb-0"> Total</p>
+
+                <p className="fw-bold">{formatCurrency(total)}</p>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={5}>
+            <form>
+              <div className="mb-3 inputRegBox">
+                <input
+                  type="text"
+                  placeholder="Fullname"
+                  id="fullname"
+                  className="w-100 mb-0 inputReg"
+                  autoFocus
+                  {...register("fullname")}
+                />
+              </div>
+              <div className="mb-3 inputRegBox">
+                <input
+                  type="text"
+                  placeholder="Address"
+                  id="address"
+                  className="w-100 mb-0 inputReg"
+                  autoFocus
+                  {...register("shippingAddress")}
+                />
+              </div>
+              <div className="mb-3 inputRegBox">
+                <input
+                  type="text"
+                  placeholder="Phone number"
+                  id="phone"
+                  className="w-100 mb-0 inputReg"
+                  autoFocus
+                  {...register("phone")}
+                />
+              </div>
+              <div className="mb-3 inputRegBox">
+                <input
+                  type="text"
+                  placeholder="State"
+                  id="state"
+                  className="w-100 mb-0 inputReg"
+                  autoFocus
+                  {...register("state")}
+                />
+              </div>
+            </form>
+          </Col>
+        </Row>
+      ) : (
+        <h1 className="text-center fs-4">
+          {" "}
+          You have no orders yet . Pls add a few items to your bag
+        </h1>
+      )}
     </PageLayout>
   );
 }
